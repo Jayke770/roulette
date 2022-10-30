6e88ccba98b33c651b99e3bb51bded5c165e1c00
Jayke770 <jakevelasco40@gmail.com>
2022-10-30 16:07:25 +0800

    .

--- pages/api/roulette/[id].ts
+++ pages/api/roulette/[id].ts
@@ -1,5 +1,4 @@
 import { NextApiRequest, NextApiResponse } from 'next'
-import dbConnect from '../../../lib/DB/connect'
 import { Roulette } from '../../../models'
 interface ExtendedNextApiRequest extends NextApiRequest {
     query: {
@@ -10,7 +9,6 @@ export default async function RouletteData(req: ExtendedNextApiRequest, res: Nex
     const { method, query: { id } } = req
     try {
         if (method === 'GET' && id) {
-            await dbConnect()
             const ROULETTES = id === 'all' ? await Roulette.find().sort({ _id: -1 }) : await Roulette.findOne({ id: id })
             return res.send(ROULETTES)
         } else {
